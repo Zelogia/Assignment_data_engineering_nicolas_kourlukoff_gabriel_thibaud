6abe7eb58f4b71ac2eceddb2151a522d8207cb69
GT2017 <63583056+GT2017@users.noreply.github.com>
2022-02-12 10:36:44 +0100

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react/cjs/react.production.min';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:3001")
+        .then(res => res.json())
+        .then(res => this.setState({ apiResponse: res.pageCount }))
+        .catch(err=>err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render () {
+    return <div className="App">
+        <p className="App-intro">the count is: {this.state.apiResponse}</p>
     </div>
-  );
-}
+  }
 
+}
 export default App;
